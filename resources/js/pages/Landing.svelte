330521d15e4c7f8b38532dee0e839a900e946d5c
AdamDyrda <a.dyrda102@gmail.com>
2025-11-29 23:31:34 +0100

    Progress on frontpage

--- resources/js/pages/Landing.svelte
+++ resources/js/pages/Landing.svelte
@@ -1,7 +1,28 @@
 <script>
-    let { isLoggedIn } = $props()
-</script>
+    import { Button } from "@/components/ui/button";
+    
+    // 1. FIXED IMPORT: Usually you import from the folder, not the specific .svelte file
+    import * as Avatar from "@/components/ui/avatar"; 
+    import { Link } from "lucide-svelte";
 
+    let { isLoggedIn } = $props();
+</script>
+<header class="flex w-full items-center justify-between p-4 border-b min-h-18">
+    <div class="absolute top-4 right-4 z-10">
+
+    {#if !isLoggedIn}
+    <Button href={route('register')}>register</Button>
+    <Button href={route('login')}>login</Button>
+    {:else}
+        <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />   
+            <Avatar.Fallback>CN</Avatar.Fallback>
+            <!-- <Link href={route('profile')}></Link> -->
+        </Avatar.Root>
+    {/if}
+    </div>
+
+</header>
 <main>
     <p> this is the initial landing page Hello :P</p>
     <p>{isLoggedIn ? "EZ" : ":("}</p>
---
+++ resources/js/pages/Profile.svelte
@@ -0,0 +1,8 @@
+<script lang='ts'>
+    import {Button} from "@/components/ui/button"
+</script>
+
+<div>
+    <p>This is profile page</p>
+    <Button href={route('register')}>Home</Button>
+</div>
