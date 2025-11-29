bf989d82f81b3382fd4e5a8ae46b6ab91ce60c09
AdamDyrda <a.dyrda102@gmail.com>
2025-11-29 14:38:10 +0100

    created landing page file and route to it

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
+Route::get('/landing', function (){
+    return Inertia::render('Landing');
+});
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
