676624ca62c2010af32ba184cc93b5569f620a1f
AdamDyrda <a.dyrda102@gmail.com>
2025-11-30 11:22:05 +0100

    finished

--- resources/js/pages/Profile.svelte
+++ resources/js/pages/Profile.svelte
@@ -1,15 +1,20 @@
 <script lang='ts'>
     import {Button} from "@/components/ui/button";
     import * as Avatar from "@/components/ui/avatar";
-    let ;
+    let {user}=$props();
 </script>
 
-<div class="align-center">
-    <div>
-        
+<div class="flex justify-center h-screen">
+    <div  class="flex align-center flex-col">
+    <div class="mx-auto">
+        <Avatar.Root class="h-[20vw] w-[20vw] min-h-12 min-w-12">
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>prof</Avatar.Fallback>
+        </Avatar.Root>
     </div>
-    <h1>{username}</h1>
-    <p>This is profile page</p>
-    <Button href={route('home')}>Home</Button>
-    <Button href={route('logout')}>Logout</Button>
+    <h1 class="text-center">Hello {user.username}!</h1>
+    <h2>This is your profile page</h2>
+    <Button href={route('home')} class="m-4">Home</Button>
+    <Button href={route('logout')} class="m-4">Logout</Button>
+</div>
 </div>
