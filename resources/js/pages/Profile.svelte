2495a1e159132fc0ff481d3d6d11740e2482cc45
AdamDyrda <a.dyrda102@gmail.com>
2025-11-30 11:11:58 +0100

    WIP profile

--- resources/js/pages/Profile.svelte
+++ resources/js/pages/Profile.svelte
@@ -1,8 +1,14 @@
 <script lang='ts'>
-    import {Button} from "@/components/ui/button"
+    import {Button} from "@/components/ui/button";
+    import * as Avatar from "@/components/ui/avatar";
+    let ;
 </script>
 
-<div>
+<div class="align-center">
+    <div>
+        
+    </div>
+    <h1>{username}</h1>
     <p>This is profile page</p>
     <Button href={route('home')}>Home</Button>
     <Button href={route('logout')}>Logout</Button>
